bf3ed24c456de656a01157b6acf1d4dde5dcdbc6
Joshua Lucas <joshuamlucas91@gmail.com>
2019-12-05 17:22:32 -0500

    add css and layout header

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,50 +1,59 @@
 <!DOCTYPE html>
 <html >
     <head>
- 
-
         <title>Joshua Lucas</title>
+
+        <!-- CSS Style Sheets -->
+        <link rel="stylesheet" href="CSS/default.css"/>
+        <link rel="stylesheet" href="CSS/fonts.css"/>
+
 
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        <!-- IF I NEED A LOGGIN/REGISTRATION FEATURE  -->
+            <!-- <div class="flex-center position-ref full-height">
+                @if (Route::has('login'))
+                    <div class="top-right links">
+                        @auth
+                            <a href="{{ url('/home') }}">Home</a>
+                        @else
+                            <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}">Register</a>
+                            @endif
+                        @endauth
+                    </div>
+                @endif
+            </div>     -->
+       
         <header>
-            <div>
-                <div>
+            <div class="main-header">
+                <div class='brand'> 
 
-                    <h2>Joshua <img src="" alt="(logo)"> Lucas</h2>
+                    <h3>Joshua <img src="" alt="(logo)"> Lucas</h3>
 
                 </div>  
                 
-                <div>
-                    <nav>
-                        <ul>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Testimonials</a></li>
-                            <li><a href="#">Projects</a></li>
-                            <li><a href="#">Contact</a></li>    
-                        </ul>
-                    
-                    </nav>
+                <div class='main-nav'>
+                    <ul>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Testimonials</a></li>
+                        <li><a href="#">Projects</a></li>
+                        <li><a href="#">Contact</a></li>    
+                    </ul>
                 </div> 
             </div>
         
         </header>
+        <div>
+         @yield('content')
         </div>
+        <footer>
+        
+            <p>created by Joshua M. Lucas all rigths reserved. All parts of this page can not be reporduced for any reason unless granted permission from owner.</p> 
 
-        @yield('content')
+        </footer>
+
     </body>
 </html>
