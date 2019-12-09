11099d5c5e25a613ec47be9f26044446fedaef05
Joshua Lucas <joshuamlucas91@gmail.com>
2019-12-08 19:23:56 -0500

    editing the typography on welcome page

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -37,7 +37,7 @@
                 
                 <div class='main-nav' >
                     <ul class='nav'>
-                        <li><a href="#">About</a></li>
+                        <li><a href="#">Bio</a></li>
                         <li><a href="#">Testimonials</a></li>
                         <li><a href="#">Projects</a></li>
                         <li><a href="#">Contact</a></li>    
