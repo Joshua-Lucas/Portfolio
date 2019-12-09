f3dc63d5803f5fa818abd03f67a4ad4a5d854a12
Joshua Lucas <joshuamlucas91@gmail.com>
2019-12-09 14:06:52 -0500

    complete the the welcome page layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,10 +8,10 @@
         <div class='lp-text'>
             <h1 class='lp_title'>Joshua Lucas</h1>
             <!-- <h3 class="lp_h3"> My name is Joshua</h3>-->
-            <p calss="lp_p"> Front-end Developer</p> 
+            <p class="lp_p"> Front-end Developer</p> 
         </div>            
             
-        <div class='lp-img'> 
+        <div class='lp_img'> 
             <img class='jlu1' src="img/LP/JLu_1.jpg" alt="Image of Joshua Lucas" >
         </div>
 
